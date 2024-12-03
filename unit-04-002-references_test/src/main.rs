9788009777bd2cc6d6793d7463e24771457e18bd
yingjie.zhao <zhaoyingjie1988@126.com>
2024-12-03 17:29:54 +0800

    所有权的引用

--- unit-04-002-references_test/src/main.rs
+++ unit-04-002-references_test/src/main.rs
@@ -3,16 +3,26 @@
 // 引用：像一个指针，因为它是一个地址，我们可以由此访问存储与该地址的属于其他变量的数据。与指针不同，引用确保指向某个特定类型的有效值
 // 借用：创建一个 引用 的行为
 fn main() {
-    variable_give_and_back_test();
+    // variable_give_and_back_test();
+    // references_change_test_err();
 }
 
 // 一个新的 calculate_length 示例，将 原来实现中 变量声明和函数返回值中的元组代码都去除掉
-fn variable_give_and_back_test() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);            // & 符号就是引用，它们允许你使用值但不获取所有权。 传递值用 &s1
-    println!("The length of '{s1}' is {len}")
-}
+// fn variable_give_and_back_test() {
+//     let s1 = String::from("hello");
+//     let len = calculate_length(&s1);            // & 符号就是引用，它们允许你使用值但不获取所有权。 传递值用 &s1
+//     println!("The length of '{s1}' is {len}")
+// }
 
-fn calculate_length(s: &String) -> usize {              // & 符号就是引用，它们允许你使用值但不获取所有权。获取值用 &String。s 是 String 的引用
-    s.len()
-}                                                       // 这里，s 离开了作用域。但因为他并不拥有引用值得所有权，所以什么也没发生。
+// fn calculate_length(s: &String) -> usize {              // & 符号就是引用，它们允许你使用值但不获取所有权。获取值用 &String。s 是 String 的引用
+//     s.len()
+// }                                                       // 这里，s 离开了作用域。但因为他并不拥有引用值得所有权，所以什么也没发生。
+
+// 会报错。 变量默认是不可变的，引用也一样。（默认）不允许修改引用的值
+// fn references_change_test_err() {
+//     let s = String::from("hello");
+//     change_err(&s);
+// }
+// fn change_err(some_string: &String) {
+//     some_string.push_str(", world");        // `some_string` is a `&` reference, so the data it refers to cannot be borrowed as mutable
+// }
